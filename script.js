72ca1014b2f1565aa0c9d4531ca8cb0a3b3c8165
Diaa Mohammed <diaam852@gmail.com>
2024-09-28 20:37:08 +0300

    edit url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/users';
+const API_URL = 'https://quran-ojzc.onrender.com/users';
 
 // Fetch users from server and display in the table
 async function fetchUsers() {
